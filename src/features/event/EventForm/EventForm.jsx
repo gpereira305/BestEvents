831b56c1715c81f1ec2ee2783ab45e6410d56f99
Giovane Pereira <giovane305pereira@gmail.com>
2020-12-03 08:25:33 -0300

    delete implementation

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -8,20 +8,36 @@ import { Button, Form, Segment } from 'semantic-ui-react';
           city: '',
           venue: '',
           hostedBy: ''
+     };
+
+
+     componentDidMount(){
+       if(this.props.selectedEvent !== null) {
+         this.setState({
+           ...this.props.selectedEvent 
+         })
+       }
      }
-
-
    
+
      handleFormSubmit = (e) => {
        e.preventDefault();
-       this.props.createEvent(this.state);
+       if(this.state.id){
+         this.props.updateEvent(this.state);
+       }else{
+        this.props.createEvent(this.state);
+       }
      }
+
 
      handleInputChange = ({target: {name, value}}) => {
         this.setState({
           [name]: value
         })
      }
+
+
+
 
     render() {
       const {cancelFormOpen} = this.props;
--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -6,7 +6,7 @@ import EventListAttendee from './EventListAttendee';
 
 class EventListItem extends Component {
     render() {
-        const {event} = this.props; 
+        const {event, selectEvent, deleteEvent} = this.props; 
 
         return (
                  <Segment.Group>
@@ -45,7 +45,19 @@ class EventListItem extends Component {
                     </Segment>
                     <Segment clearing>
                       <span>{event.description}</span>
-                      <Button as="a" color="teal" floated="right" content="View" />
+                      <Button as="a"
+                        onClick={() => deleteEvent(event.id)}
+                        color="red" 
+                        floated="right"
+                        content="Delete"
+                      />
+                      
+                      <Button as="a"
+                        onClick={() => selectEvent(event)}
+                        color="teal" 
+                        floated="right"
+                        content="View"
+                      />
                     </Segment>
                   </Segment.Group>
         )
